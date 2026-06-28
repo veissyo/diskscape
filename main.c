db4fedf55a6f9a83c6645d2e839787a6bf6a054d
veissyo <martyna.danilczykk6@gmail.com>
2026-06-28 17:40:57 +0200

    Add support for `--hide-empty` and `--top` command-line options

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "diskscape.h"
 
 int main(int argc, char *argv[]) {
+    int hide_empty = 0;
+    int top = 0;
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "--hide-empty") == 0) hide_empty = 1;
+        if (strcmp(argv[i], "--top") == 0) {
+            top = atoi(argv[++i]);
+        }
+    }
     Entry entries[256];
     char buf[32];
     int count = 0;
@@ -12,9 +21,11 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < count; i++) {
         char buf[32];
         format_size(entries[i].size, buf);
+        if (hide_empty && entries[i].size == 0) continue;
         printf("%-10s ", entries[i].name);
         print_bar(i, count, entries);
         printf("  %s\n", buf);
+        if (top > 0 && i >= top - 1) break;
     }
     return 0;
 }
